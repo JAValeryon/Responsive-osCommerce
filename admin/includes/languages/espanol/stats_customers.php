cb3871a01c19d056595723d0beec1fe7889e2a3b
Alex <JAValeryon@users.noreply.github.com>
2018-04-18 13:22:18 +0200

    Add language espanol to admin

---
+++ admin/includes/languages/espanol/stats_customers.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2013 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Clientes por Total de Pedido');
+
+define('TABLE_HEADING_NUMBER', 'Número');
+define('TABLE_HEADING_CUSTOMERS', 'Clientes');
+define('TABLE_HEADING_TOTAL_PURCHASED', 'Total Pedido');
+?>

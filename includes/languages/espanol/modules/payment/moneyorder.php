3c0c89c59ea441f2a2499e4f055f7e766c6be229
Alex <JAValeryon@users.noreply.github.com>
2018-05-08 13:04:20 +0200

    Fix warning message when installing new payment modules

--- includes/languages/espanol/modules/payment/moneyorder.php
+++ includes/languages/espanol/modules/payment/moneyorder.php
@@ -11,6 +11,13 @@
 */
 
   define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Cheque / Giro postal');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Hacer pagadero a:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br /><br />Enviar a:<br />' . STORE_NAME . '<br />' . nl2br(STORE_ADDRESS) . '<br /><br />' . 'Su pedido no se enviará hasta que recibamos el pago.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Hacer pagadero a: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nEnviar a:\n" . STORE_NAME . "\n" . STORE_ADDRESS . "\n\n" . 'Su pedido no se enviará hasta que recibamos el pago.');
+  
+  if ( defined('MODULE_PAYMENT_MONEYORDER_STATUS') ) {  
+    define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Hacer pagadero a:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br /><br />Enviar a:<br />' . STORE_NAME . '<br />' . nl2br(STORE_ADDRESS) . '<br /><br />' . 'Su pedido no se enviará hasta que recibamos el pago.');
+    define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Hacer pagadero a: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nEnviar a:\n" . STORE_NAME . "\n" . STORE_ADDRESS . "\n\n" . 'Su pedido no se enviará hasta que recibamos el pago.');
+  }
+  else {
+    define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Hacer pagadero a:&nbsp;<br /><br />Enviar a:<br />' . STORE_NAME . '<br />' . nl2br(STORE_ADDRESS) . '<br /><br />' . 'Su pedido no se enviará hasta que recibamos el pago.');
+    define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Hacer pagadero a: \n\nEnviar a:\n" . STORE_NAME . "\n" . STORE_ADDRESS . "\n\n" . 'Su pedido no se enviará hasta que recibamos el pago.');
+  }
 ?>

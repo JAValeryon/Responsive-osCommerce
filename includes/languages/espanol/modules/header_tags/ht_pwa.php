e636f82207554fec898fcd7a7d6054db72edbb62
Alex <JAValeryon@users.noreply.github.com>
2018-05-31 10:49:15 +0200

    Fix typo in PWA module

--- includes/languages/espanol/modules/header_tags/ht_pwa.php
+++ includes/languages/espanol/modules/header_tags/ht_pwa.php
@@ -20,7 +20,7 @@
   define('MODULE_HEADER_TAGS_PWA_DESCRIPTION', 'Añadir scripts para PWA cuenta de invitado.');
 
   define('MODULE_HEADER_TAGS_PWA_LOGIN_WARNING', '<strong>El Módulo de Contenido "PWA Login para Invitados"</strong> no está instalado o no está habilitado.<br>Es requerido, por favor instálelo.');
-  define('MMODULE_HEADER_TAGS_PWA_INSTALL_NOW', '<u>Instalar Ahora</u>');
+  define('MODULE_HEADER_TAGS_PWA_INSTALL_NOW', '<u>Instalar Ahora</u>');
 
   define('MODULE_HEADER_TAGS_PWA_TEXT_PRODUCTS', '<strong>Producto(s) comprado(s:</strong>');
   define('MODULE_HEADER_TAGS_PWA_DOWNLOAD', '<br>Puede descargar sus productos más adelante en su Cuenta en la página "Ver pedidos", si opta por una cuenta permanente configurando su contraseña.');
